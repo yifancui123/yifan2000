95c6850c1f15a1b6c9669cfce5b21a97301425d7
Yifan Cui <yifan.cui@students.mq.edu.au>
2025-09-02 15:11:30 +1000

    add trail comment

--- src/StageReader.java
+++ src/StageReader.java
@@ -9,4 +9,23 @@ public class StageReader {
     List<String> lines = Files.readAllLines(Paths.get(path));
     return new Stage();
   }
+
 }
+/*
+ * try {
+ *  readFromFile("stage1.rvb");
+ * }
+ * catch (FileNotFoundException e){
+ *  if (file == null)
+ *  System.out.println("files not found");
+ * }
+ * 
+ * catch (IOException e){
+ *  if (grid.row != ...)
+ * }
+ * 
+ * catch (Exception e){
+ *   if (0 < row || row > 21)
+ *      System.out.println("out of bonder");
+ * }
+ */
